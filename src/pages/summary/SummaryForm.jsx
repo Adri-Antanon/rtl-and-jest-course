782691844b2c7e0d067c86e1b3db00f93083b03b
Adrian Antanon Orozco <adrianantanon@gmail.com>
2022-04-01 15:48:14 +0200

    test: summary form checkbox & button

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import { useState } from 'react';
+import Form from 'react-bootstrap/form';
+import Button from 'react-bootstrap/button';
 
 const SummaryForm = () => {
-  return <div>SummaryForm</div>;
+  const [isChecked, setIsChecked] = useState(false);
+
+  const checkboxLabel = (
+    <span>
+      I agree to the <span>Terms and Conditions</span>
+    </span>
+  );
+
+  return (
+    <Form>
+      <Form.Group controlId="terms-and-conditions">
+        <Form.Check
+          type="checkbox"
+          checked={isChecked}
+          onChange={(event) => setIsChecked(event.target.checked)}
+          label={checkboxLabel}
+        />
+      </Form.Group>
+      <Button type="submit" variant="primary" disabled={!isChecked}>
+        Confirm Order
+      </Button>
+    </Form>
+  );
 };
 
 export default SummaryForm;
