499d05997e9ef7c51609db7a435717fac6ed9751
Adrian Antanon Orozco <adrianantanon@gmail.com>
2022-04-10 18:56:30 +0200

    test: topping options from server

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 import ScoopOption from './ScoopOption';
 import Row from 'react-bootstrap/Row';
+import ToppingOption from './ToppingOption';
 
 const Options = ({ optionType }) => {
   const [items, setItems] = useState([]);
@@ -20,7 +21,7 @@ const Options = ({ optionType }) => {
   }, [optionType]);
 
   //TODO: replace 'null' with ToppingOption when available
-  const ItemComponent = optionType === 'scoops' ? ScoopOption : null;
+  const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption;
 
   const optionItems = items.map((item) => (
     <ItemComponent
--- src/pages/entry/__test__/Options.test.jsx
+++ src/pages/entry/__test__/Options.test.jsx
@@ -13,3 +13,21 @@ test('displays image for each scoop option from server', async () => {
   const altText = scoopImages.map((image) => image.getAttribute('alt'));
   expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
 });
+
+test('Displays image for each topping option from server', async () => {
+  render(<Options optionType="toppings" />);
+  const toppingImages = await screen.findAllByRole('img', {
+    name: /topping$/i,
+  });
+  //   expect(toppingImages.length).toBe(3);
+  // find images
+  expect(toppingImages).toHaveLength(3);
+
+  // Check the actual alt text for the images
+  const altText = toppingImages.map((image) => image.getAttribute('alt'));
+  expect(altText).toEqual([
+    'Cherries topping',
+    'M&Ms topping',
+    'Hot fudge topping',
+  ]);
+});
